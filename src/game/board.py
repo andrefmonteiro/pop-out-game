60691d6a09686be3d2c4ce0c25db91c4b762ce4a
Andre Monteiro <andrefmonteiro@outlook.com>
2026-04-14 10:58:29 +0100

    str method overloaded for better printing viz

--- src/game/board.py
+++ src/game/board.py
@@ -1,4 +1,5 @@
 import numpy as np
+from move import Move
 
 """
 get_winner() → int | None # avaliar resultado
@@ -14,14 +15,27 @@ pass the player's Move and if it yields True, then we apply the move
 
 
 class Board:
+
     def get_winner(self):
         # returns 1, 2 or 0 (in case of a tie)
         pass
 
-    def apply_move(move: Move):
+    def apply_move(self, move: Move):
         pass
+
+    def __str__(self):
+        # mapping symbols to states for a better print
+        symbols = {0: ".", 1: "X", 2: "0"}
+        rows = []
+        for row in self.grid:
+            rows.append(" ".join(symbols[cell] for cell in row))
+        return "\n".join(rows)
 
     def __init__(self):
         # grid with numpy array: 6 height * 7 width - it's indexed by a tuple of ints
         self.current_player = 1
         self.grid = np.zeros((6, 7), np.uint8)
+
+
+b = Board()
+print(b)
